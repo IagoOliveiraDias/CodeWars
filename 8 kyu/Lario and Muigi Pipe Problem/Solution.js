13a3c862283f81329a4379f17d51fa72ca871964
Iago Oliveira Dias <iagodois@hotmail.com>
2022-04-22 20:49:43 -0300

    Lario and Muigi Pipe problem

---
+++ 8 kyu/Lario and Muigi Pipe Problem/Solution.js
@@ -0,0 +1,9 @@
+function pipeFix(numbers){
+    let newarray = [];
+      
+    for (let i = numbers[0]; i <= numbers[numbers.length-1]; i++){
+      newarray.push(i);
+      }
+      return newarray;
+    }
+    
